db4f3ffde6079feb0f1e229f20e545771c3d8b08
weigaosS <wei.gao@china.talkdesk.com>
2023-04-26 12:25:53 +0800

    change config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// eslint-disable-next-line no-undef
+const isGitHub = process.env.TARGET === "github";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: isGitHub ? "/TechNiHao/" : "/",
+});
